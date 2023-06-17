39f246e9ab0acb407c7298423cd48d669cc19405
Ruben <86058977+RubenShaw1@users.noreply.github.com>
2023-06-17 16:00:13 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import requests
+
+
+bot = commands.Bot(command_prefix='/')
+
+token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+shlink_api_key = 'null'
+
+@bot.event
+async def on_ready():
+    print(f'Im ready! Logged in as {bot.user.name}')
+
+
+@bot.event
+async def on_member_join(member):
+    channel = member.guild.system_channel
+    if channel:
+        await channel.send(f'Welcome, {member.mention}.')
+
+
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member):
+    await member.kick()
+    await ctx.send(f'{member.mention} has been kicked.')
+
+
+@bot.command()
+@commands.has_role('Owner')
+async def announce(ctx, *, announcement: str):
+    author = ctx.message.author
+    await ctx.send(f'{author.name} wants everyone to know that {announcement}')
+
+
+
+bot.run(token)
+
+
+
+
